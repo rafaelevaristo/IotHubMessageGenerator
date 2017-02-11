db2795918c8cd79ee639c36b464101d108378a6a
Rafael Evaristo <elastico@outlook.com>
2017-02-11 16:04:22 +0000

    Initial commit

---
+++ CreateDeviceIdentity/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+using System;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace IotHubMessageGenerator
+{
+    class Program
+    {
+
+        static RegistryManager registryManager;
+        static string connectionString = ConfigurationManager.AppSettings["connectionString"];
+
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "myFirstDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+
+        static void Main(string[] args)
+        {
+
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+IOT Eventhub usage example !
+===================
+
+Based on  [^Get started with Azure IoT Hub (.NET)]: [Get started with Azure IoT Hub (.NET)](https://docs.microsoft.com/en-us/azure/iot-hub/iot-hub-csharp-csharp-getstarted) 
+
+----------
+
+> **Note:** You can find cool resources:
+
+> - about **Markdow Editor** [here][1],
+
+
+  [1]: https://stackedit.io/ "Markdow editor"
+ 
